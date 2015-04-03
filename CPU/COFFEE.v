9fb8b15e983e51577f219fbcd671719d060970e0
Mathias Helsen <helsen.mathias@gmail.com>
2015-04-03 16:25:27 +0200

    somewhat more complicated CPU already...

--- CPU/COFFEE.v
+++ CPU/COFFEE.v
@@ -1,11 +1,32 @@
-module COFFEE(input CLK50);
+module COFFEE(input CLK50, 
+    output LEDG[9:0], 
+    output HEX0_D[6:0], 
+    output HEX1_D[6:0],
+    output HEX2_D[6:0],
+    output HEX3_D[6:0]);
 
-wire address
+wire [15:0]address;
+wire [31:0]data;
+wire [31:0]q;
+wire [7:0]status;
 
-module memory (
-	address,
-	clock,
+wire clock;
+wire wren;
+
+assign LEDG[7:0] = status;
+
+memory mem (
+	{1'b0, address[14:0]},
+	!clock,
 	data,
 	wren,
 	q);
-endmodule;
+
+CPU cpu(data, 
+    q, 
+    address,
+    wren, 
+    clock,
+    status);
+
+endmodule
--- CPU/CPU.v
+++ CPU/CPU.v
@@ -1,21 +1,29 @@
-module CPU(output data[15:0], input q[15:0], output wren, input clk, output [7:0]status);
+module CPU(output [31:0]data, input [31:0]q, output [15:0]address, output wren, input clk, output [7:0]status);
 
-`define NOP    8h0;
-`define LOAD   8h1;
-`define STORE  8h2;
-`define LOADLI 8h3;
-`define LOADHI 8h4;
-`define JUMPZ  8h5;
+`define NOP    8'h0;
+`define LOAD   8'h1;
+`define STORE  8'h2;
+`define LOADLI 8'h3;
+`define LOADHI 8'h4;
+`define JUMPZ  8'h5;
 
-`define MOV 8h6;
-`define AND 8h7;
-`define OR  8h8;
-`define XOR 8h9;
-`define ADD 8hA;
+`define MOV 8'h6;
+`define AND 8'h7;
+`define OR  8'h8;
+`define XOR 8'h9;
+`define ADD 8'hA;
+
+`define FETCH	8'h0;
+`define DECODE	8'h1;
+`define EXECUTE	8'h2;
 
 
 reg [15:0] pc;
 reg [31:0]r[7:0];
+reg [31:0] command;
+
+reg [7:0] state;
+
 wire [31:0]command;
 wire [7:0]op;
 wire [7:0]r1;
@@ -23,26 +31,51 @@ wire [7:0]r2;
 wire [7:0]r3;
 wire [15:0]addr;
 
-assign op = memory[pc][31:24];
-assign r1 = memory[pc][23:16];
-assign r2 = memory[pc][15:8];
-assign r3 = memory[pc][7:0];
-assign addr = memory[pc][15:0];
+assign op = command[31:24];
+assign r1 = command[23:16];
+assign r2 = command[15:8];
+assign r3 = command[7:0];
+assign addrOp = command[15:0];
 
 always @(posedge clk) begin
-    case (op)
-	`LOAD: r1 <= memory[addr];
-	`STORE: memory[addr] <= r1;
-	`MOV: r2 <= r1;
-	`LOADLI: r1 <= {r1[31:16], addr};
-	`LOADHI: r1 <= {addr, r1[15:0]};
-	default: ;
+    case(state)
+	`FETCH: begin
+	    command <= q;
+	    state <= `DECODE;
+	end
+	`DECODE: begin
+	    state <= `FETCH;
+	    case (op)
+		`LOAD: begin
+		    address <= addrOp;
+		    wren <= 1'b0;
+		    state <= `LOAD1;
+		end
+		`STORE: begin
+		    address <= addrOp;
+		    wren <= 1'b1;
+		    data <= r[r1];
+		end
+		`MOV: r2 <= r1;
+		`LOADLI: r1 <= {r1[31:16], addr};
+		`LOADHI: r1 <= {addr, r1[15:0]};
+		default: ;
+	    endcase
+	end
+	`EXECUTE: begin
+	    case(op)
+		`LOAD: begin
+		    state <= `FETCH;
+		   r[r1] <= q;
+		end
+		default: begin
+		    address <= pc;
+		    wren <= 1'b0;
+		    state <= `FETCH;
+		end
+	    endcase
+	end
     endcase
-    if( op == `JMPIFZERO ) begin
-	pc <= pc + r1[15:0];				
-    end else begin
-	pc <= pc + 16'h01;
-    end
 end
 
 endmodule;
